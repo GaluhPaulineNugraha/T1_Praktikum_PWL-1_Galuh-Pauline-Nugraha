dd6fc07305002809ff1b6949ad5d4074062eae93
Galuh Pauline Nugraha <paulinenugrahagaluh@gmail.com>
2026-04-14 21:27:38 +0700

    commit Model ERD

---
+++ app/Models/Loan.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Loan extends Model
+{
+    protected $fillable = [
+        'user_npm', 'loan_at', 'return_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_npm');
+    }
+
+    public function loanDetails()
+    {
+        return $this->hasMany(LoanDetail::class);
+    }
+}
